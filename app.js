446e46d8533fae25325525b3cb03ec2516a9a9bb
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-11-11 17:27:15 +0100

    Create routes for faker store generation

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var storesRouter = require('./routes/stores');
+
+
+/* Dummy Data */
+var dummyData = require('./utils/data');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
-
 
 
 // view engine setup
@@ -23,33 +27,34 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/stores', storesRouter);
+app.use('/data', dummyData);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
-
 //localhost:3003
-app.listen(3003, ()=>{
+app.listen(3003, () => {
     console.log("Server is up and listening on 3003...")
 })
 
