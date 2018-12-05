04d62a6216a7d293217240503ab96f795f90c178
Ben Helal Wajdi <wajdibenhelal52@live.fr>
2018-12-05 21:06:21 +0100

    add method for add the product to the store with id of the store add method for update the product by the id of this product add method for delete the product with the id of this product

--- routes/products.js
+++ routes/products.js
@@ -20,6 +20,59 @@ router.get('/:id', (req, res) => {
     })
 });
 
+/*Add Product by Store*/
+//id	name	description	price	quantity	image	size	store_id
+router.get('/:name/:description/:price/:quantity/:image/:size/:store_id', (req, res)=>{
+    console.log("Add product to the storeID : "+ req.params.store_id);
+    console.log("Product with name : "+ req.params.name +" , description : "+ req.params.description +" , price : "+req.params.price+ " , quantity :" + req.params.quality + " , image : " + req.params.image +  " and size :" + req.params.size + " ,");
+    const queryString = "INSERT INTO product (name, description, price, quantity, image, size, store_id) value (?,?,?,?,?,?,?)";
+    getConnection().query(queryString, [req.params.name, req.params.description, req.params.price, req.params.quantity,
+        req.params.image, req.params.size, req.params.store_id ],(err, results, fields) => {
+        if (err) {
+            console.log("Failed to insert new product: " + err);
+            res.sendStatus(500);
+            return
+        }
+        console.log("Inserted a new product with id :" + results.insertId);
+        res.end();
+    })
+});
+
+/*Update Product by id */
+//id	name	description	price	quantity	image	size	store_id
+router.get('/:name/:description/:price/:quantity/:image/:size/:store_id/:id', (req, res)=>{
+    console.log("Add product to the storeID : "+ req.params.store_id);
+    console.log("Product with name : "+ req.params.name +" , description : "+ req.params.description +" , price : "+req.params.price+ "" +
+        " , quantity :" + req.params.quantity + " , image : " + req.params.image +  " and size :" + req.params.size + " ,");
+    const queryString = "update product set name = ? , description = ? , price = ? , quantity = ? , image = ? , size = ?, store_id = ? where id = ? ";
+    getConnection().query(queryString, [req.params.name, req.params.description, req.params.price, req.params.quantity,
+        req.params.image, req.params.size, req.params.store_id, req.params.id ],(err, results, fields) => {
+        if (err) {
+            console.log("Failed to update a product: " + err);
+            res.sendStatus(500);
+            return
+        }
+        console.log("update a product with id :" + results.insertId);
+        res.end();
+    })
+});
+
+//delete product
+//DELETE FROM `product` WHERE `id` = 13
+router.get('/:store_id/:id', (req, res)=>{
+    console.log("Add product to the storeID : "+ req.params.store_id);
+    console.log("Product with id : "+req.params.id+ " ");
+    const queryString = "delete from  product where id = ? ";
+    getConnection().query(queryString, [req.params.id ],(err, results, fields) => {
+        if (err) {
+            console.log("Failed to delete a product: " + err);
+            res.sendStatus(500);
+            return
+        }
+        console.log("delete product  :" + req.id);
+        res.end();
+    })
+});
 
 var pool = mysql.createPool({
     host: 'localhost',
