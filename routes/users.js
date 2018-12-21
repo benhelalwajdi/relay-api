6d4263b8b7d3fd6c08bda94f96c209737a2a52a5
Ahlem Ben Guiza <ahlembenguiza@gmail.com>
2018-12-21 13:57:58 +0100

    Create route to add new client

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,6 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var router = express.Router();
 
-
-/* GET users listing. */
-/*router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-*/
-
 /* Login */
 router.get('/user/:mail/:password', (req, res) => {
     var bool;
@@ -41,50 +34,25 @@ router.get('/user/:mail/:password', (req, res) => {
     });
 });
 
-/* Get user by mail. */
-/*router.get('/user/:mail', (req, res) => {
-    console.log("Fetching user with mail: " + req.params.id);
-
-    const userMail = req.params.mail;
-    const queryString = "SELECT * FROM user WHERE mail = ?";
-    getConnection().query(queryString, [userMail], (err, rows, fields) => {
+/* Create new client */
+router.post('/create_client', (req, res) => {
+    let password = bcrypt.hashSync(req.params.password, bcrypt.genSaltSync(10));
+    const queryString = "INSERT INTO user (first_name, last_name, mail, password, phone_number," +
+        " address, user_type, creation_date) VALUES (?,?,?,?,?,?,?,?)";
+    getConnection().query(queryString, [req.body.first_name, req.body.last_name, req.body.mail, password,
+        req.body.phone_number, req.body.address, 'CLIENT', new Date()], (err, results, fields) => {
         if (err) {
-            console.log("Failed to query for users: " + err);
+            console.log("Failed to insert new client: " + err);
             res.sendStatus(500);
             return
         }
-        console.log("User fetched successfully");
-        /*
-                //Format JSON
-                const users = rows.map((row)=>{
-                    return {email: row.mail}
-                })
-        */
-/*        res.json(rows)
-    })
-});
-*/
-
-
-/* Create new user. */
-router.post('/create_client/:first_name/:last_name/:password/:phone_number/:address/:user_type', (req, res) => {
-    let mail = req.params.mail;
-    let password = bcrypt.hashSync(req.params.password, bcrypt.genSaltSync(10));
-        const queryString = "INSERT INTO user (first_name, last_name, mail, password, phone_number," +
-            " address, user_type, creation_date) VALUES (?,?,?,?,?,?,?,?)";
-        getConnection().query(queryString, [req.params.first_name, req.params.last_name, mail, password,
-            req.params.phone_number, req.params.address, req.params.user_type, new Date()], (err, results, fields) => {
-            if (err) {
-                console.log("Failed to insert new user: " + err);
-                res.sendStatus(500);
-                return
-            }
-            console.log("Inserted a new user with id :" + results.insertId);
-            res.end();
-        });
+        console.log("Inserted a new client with id :" + results.insertId);
+        res.end();
+    });
     console.log("Email " + req.params.mail);
     res.end();
 });
+
 
 var pool = mysql.createPool({
     host: 'localhost',
