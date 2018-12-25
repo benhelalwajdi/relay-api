42b494cf5a14b9feff657af93eb0a8d22995a37e
Ben Helal Wajdi <wajdibenhelal52@live.fr>
2018-12-25 17:20:33 +0100

    paste the change from server to local branch

--- routes/products.js
+++ routes/products.js
@@ -1,26 +1,55 @@
 var express = require('express');
 var mysql = require('mysql');
-var router = express.Router();
-
 var multer = require('multer');
 
 
-/* GET product listing . */
-router.get('/', (req, res) => {
-    console.log("Fetching product  :"+ req.params.id);
+var router = express.Router();
 
-    const queryString = "SELECT * FROM product ORDER BY date desc";
+/* GET product listing */
+router.get('/', (req, res) => {
+    console.log("Fetching product :" + req.params.id);
+
+    const queryString = "SELECT * FROM product order by date desc";
+    getConnection().query(queryString, [req.params.id], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for products " + err);
+            res.json({status: false, error: err});
+        }
+        console.log("Products fetched successfully");
+        res.json(rows)
+    });
+});
+
+/* GET product by id */
+router.get('/:id', (req, res) => {
+    const queryString = "SELECT * FROM product WHERE id = ?";
+    getConnection().query(queryString, [req.params.id], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for product by ID" + err);
+            res.json({status: false, error: err});
+        }
+        console.log("Product fetched by ID successfully");
+        res.json(rows[0])
+    });
+});
+
+
+/* GET product listing by store. */
+router.get('/store/:id', (req, res) => {
+    console.log("Fetching product by storeID :" + req.params.id);
+
+    const queryString = "SELECT * FROM product WHERE store_id = ?";
     getConnection().query(queryString, [req.params.id], (err, rows, fields) => {
         if (err) {
             console.log("Failed to query for product by storeID " + err);
-            res.sendStatus(500);
-            return
+            res.json({status: false, error: err});
         }
         console.log("Products fetched by type successfully");
         res.json(rows)
-    })
+    });
 });
 
+//TODO: Check if the code below works !
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -34,8 +63,8 @@ var upload = multer({storage: storage});
 
 module.exports = upload;
 
+
 router.post('/upload',upload.single('profile'), function (req, res) {
-    message : "Error! in image upload.";
     if (!req.file) {
         console.log("No file received");
         message = "Error! in image upload.";
@@ -44,107 +73,67 @@ router.post('/upload',upload.single('profile'), function (req, res) {
     } else {
         console.log('file received');
         console.log(req.file.filename);
-       /*    var sql = "INSERT INTO user(`name`, `type`, `size`) VALUES ('" + req.file.filename + "', '"+req.file.mimetype+"', '"+req.file.size+"')";
-
-        var query = db.query(sql, function(err, result) {
-            console.log('inserted data');
-        });*/
         message = "Successfully! uploaded";
         res.render('index',{message: message, status:'success'});
     }
 });
 
-/* GET product listing by store. */
-router.get('/:id', (req, res) => {
-    console.log("Fetching product by storeID :"+ req.params.id);
-
-    const queryString = "SELECT * FROM product WHERE store_id = ?";
-    getConnection().query(queryString, [req.params.id], (err, rows, fields) => {
-        if (err) {
-            console.log("Failed to query for product by storeID " + err);
-            res.sendStatus(500);
-            return
-        }
-        console.log("Products fetched by type successfully");
-        res.json(rows)
-    })
-});
-
-/* GET product listing id prod by store. */
-
-router.get('/:store_id/:id', (req, res) => {
-    console.log("Fetching product by store_id  : "+ req.params.store_id +" and id product : "+req.params.id);
-
-    const queryString = "SELECT * FROM product WHERE store_id = ? and id = ?";
-    getConnection().query(queryString, [req.params.store_id, req.params.id], (err, rows, fields) => {
-        if (err) {
-            console.log("Failed to query for product by store_id and id " + err);
-            res.sendStatus(500);
-            return
-        }
-        console.log("Products fetched by id  successfully");
-        res.json(rows)
-    })
-});
-
-
-/*Add Product by Store*/
-router.post('/add', function (req, res){
+/* ADD new product */
+router.post('/create_product', function (req, res){
     console.log(req.headers);
     console.log(req.body);
-    const FORM_URLENCODED = 'application/form-data';
     console.log("Add product to the storeID : " + req.body.store_id);
-        console.log("Product with name : " + req.body.name + " , description : " + req.body.description +
-            " , price : " + req.body.price + " , quantity :" + req.body.quantity + " , image : " + req.body.image + " and size :" + req.body.size + " ,");
-        const queryString = "INSERT INTO product (name, description, price, quantity, image," +
-            "size, store_id) VALUES (?,?,?,?,\"https://www.magidglove.com/assets/item/hero/620_HERO.jpg\",?,?)";
-        getConnection().query(queryString, [req.body.name, req.body.description, req.body.price, req.body.quantity, req.body.size, req.body.store_id], (err, results, fields) => {
-            if (err) {
-                console.log("Failed to insert new product: " + err);
-                res.sendStatus(500);
-                return
-            }
-            console.log("Inserted a new product with id :" + results.insertId);
-            res.end();
-        })
+    console.log("Product with name : " + req.body.name + " , description : " + req.body.description +
+        " , price : " + req.body.price + " , quantity :" + req.body.quantity + " , image : " + req.body.image +
+        " and size :" + req.body.size + " ,");
+    const queryString = "INSERT INTO product (name, description, price, quantity, image," +
+        "size, store_id) VALUES (?,?,?,?,\"https://www.magidglove.com/assets/item/hero/620_HERO.jpg\",?,?)";
+    getConnection().query(queryString, [req.body.name, req.body.description, req.body.price, req.body.quantity,
+        req.body.size, req.body.store_id], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to insert new product: " + err);
+            res.json({status: false, error: err});
+        }
+        console.log("Inserted a new product with id :" + results.insertId);
+        res.json({status: true});
+    })
 });
 
-router.post('/delete', function (req, res){
+/* UPDATE product */
+router.post('/update_product', function (req, res){
     console.log(req.headers);
     console.log(req.body);
-    const FORM_URLENCODED = 'application/form-data';
-    console.log("Add product to the storeID : " + req.body.id);
+    console.log("update product to the store with ID : " + req.body.store_id);
+    console.log("Product with name : " + req.body.name + " , description : " + req.body.description +
+        " , price : " + req.body.price + " , quantity :" + req.body.quantity + " , size :" + req.body.size + " ,");
+    const queryString = "update product set name = ? , description = ? , price = ? , quantity = ? , " +
+        "size = ? where id = ?";
+    getConnection().query(queryString, [req.body.name, req.body.description, req.body.price, req.body.quantity,
+        req.body.size,  req.body.id], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to update product: " + err);
+            res.json({status: false, error: err});
+        }
+        console.log("update a  product with id :" + req.body.id);
+        res.json({status: true});
+    })
+});
+
+/* Delete product */
+router.post('/delete_product', function (req, res){
+    console.log(req.headers);
+    console.log(req.body);
+    console.log("Add product to the store with ID : " + req.body.id);
     const queryString = "delete from Product where id = ?";
     getConnection().query(queryString, [req.body.id], (err, results, fields) => {
         if (err) {
             console.log("Failed to delete product: " + err);
-            res.sendStatus(500);
-            return
+            res.json({status: false, error: err});
         }
         console.log("Delete product ");
-        res.end();
+        res.json({status: true});
     })
 });
-
-
-router.post('/update/produit', function (req, res){
-    console.log(req.headers);
-    console.log(req.body);
-    console.log("update product to the storeID : " + req.body.store_id);
-        console.log("Product with name : " + req.body.name + " , description : " + req.body.description +
-            " , price : " + req.body.price + " , quantity :" + req.body.quantity + " , size :" + req.body.size + " ,");
-        const queryString = "update product set name = ? , description = ? , price = ? , quantity = ? , size = ? where id = ?";
-        getConnection().query(queryString, [req.body.name, req.body.description, req.body.price, req.body.quantity,  req.body.size,  req.body.id], (err, results, fields) => {
-            if (err) {
-                console.log("Failed to update product: " + err);
-                res.sendStatus(500);
-                return
-            }
-            console.log("update a  product with id :" + req.body.id);
-            res.end();
-        })
-});
-
 
 
 var pool = mysql.createPool({
